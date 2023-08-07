f6748e9b64d9e23ef285cecf29512c348690367f
GiuseppePitruzzella <giuseppepitru@icloud.com>
2023-08-08 01:01:18 +0200

    :lock: Add config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-CHATGPT_API_KEY = "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"
+CHATGPT_API_KEY = "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"
 HISTORY = [{"role" : "user", "content" : "Your task is to summarise the text I have given you in up to seven concise bullet points, starting with a short highlight. Choose an appropriate emoji for each bullet point. Use the text above: {{Title}} {{Transcript}}. Write your response in Italian."}]
