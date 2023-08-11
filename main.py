cfd400330136a2f5ec7a7375c0c6f8842a262dc9
GiuseppePitruzzella <giuseppepitru@icloud.com>
2023-08-11 23:30:48 +0200

    :art: Refactor Main

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from flask_frozen import Freezer
 from Server import *
 
 server = Server()
-freezer = Freezer(server.getApp()) # Added
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "build":
-        freezer.freeze()
-    else:
-        server.startServer()
+    server.startServer()
